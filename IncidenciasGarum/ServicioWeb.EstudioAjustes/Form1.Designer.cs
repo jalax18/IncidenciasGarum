6911f1cb4f4fb4c69128bb1ef5745ae81a4e488d
Jose Antonio lax peñalver <jalax18@gmail.com>
2020-02-13 11:03:20 +0100

    antes de crear la web

---
+++ IncidenciasGarum/ServicioWeb.EstudioAjustes/Form1.Designer.cs
@@ -0,0 +1,379 @@
+﻿namespace ServicioWeb.EstudioAjustes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox14 = new System.Windows.Forms.TextBox();
+            this.label22 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.textBox12 = new System.Windows.Forms.TextBox();
+            this.label12 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(538, 67);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(168, 46);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Conectar a Base de datos";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox14
+            // 
+            this.textBox14.Location = new System.Drawing.Point(156, 18);
+            this.textBox14.Name = "textBox14";
+            this.textBox14.Size = new System.Drawing.Size(339, 20);
+            this.textBox14.TabIndex = 69;
+            // 
+            // label22
+            // 
+            this.label22.AutoSize = true;
+            this.label22.Location = new System.Drawing.Point(12, 18);
+            this.label22.Name = "label22";
+            this.label22.Size = new System.Drawing.Size(104, 13);
+            this.label22.TabIndex = 68;
+            this.label22.Text = "Estacion de Servicio";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(12, 138);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(53, 13);
+            this.label13.TabIndex = 66;
+            this.label13.Text = "Password";
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(156, 134);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(339, 20);
+            this.textBox8.TabIndex = 65;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(497, 69);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(35, 13);
+            this.label9.TabIndex = 64;
+            this.label9.Text = "label9";
+            this.label9.Visible = false;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(156, 65);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(339, 20);
+            this.textBox6.TabIndex = 63;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(12, 68);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(32, 13);
+            this.label8.TabIndex = 62;
+            this.label8.Text = "URL ";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(97, 208);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(60, 20);
+            this.textBox4.TabIndex = 57;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 211);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(74, 13);
+            this.label4.TabIndex = 56;
+            this.label4.Text = "Hora Estudio1";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(156, 41);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(95, 20);
+            this.textBox3.TabIndex = 55;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 44);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 13);
+            this.label3.TabIndex = 54;
+            this.label3.Text = "Hora Actual";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 116);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 13);
+            this.label2.TabIndex = 53;
+            this.label2.Text = "Usuario";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 92);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 13);
+            this.label1.TabIndex = 52;
+            this.label1.Text = "Ip BBDD";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(156, 111);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(339, 20);
+            this.textBox2.TabIndex = 51;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(156, 88);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(139, 20);
+            this.textBox1.TabIndex = 50;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(12, 160);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(77, 13);
+            this.label7.TabIndex = 70;
+            this.label7.Text = "Base de Datos";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(156, 157);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(339, 20);
+            this.textBox5.TabIndex = 71;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 182);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(134, 13);
+            this.label5.TabIndex = 72;
+            this.label5.Text = "Test de conexion  a BBDD";
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(156, 181);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(20, 20);
+            this.textBox7.TabIndex = 73;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(312, 91);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(38, 13);
+            this.label6.TabIndex = 74;
+            this.label6.Text = "Puerto";
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(356, 88);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(139, 20);
+            this.textBox9.TabIndex = 75;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(535, 15);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(168, 46);
+            this.button2.TabIndex = 76;
+            this.button2.Text = "Grabar Configuracion";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox10
+            // 
+            this.textBox10.Location = new System.Drawing.Point(252, 206);
+            this.textBox10.Name = "textBox10";
+            this.textBox10.Size = new System.Drawing.Size(81, 20);
+            this.textBox10.TabIndex = 78;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(172, 211);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(74, 13);
+            this.label10.TabIndex = 77;
+            this.label10.Text = "Hora Estudio2";
+            // 
+            // textBox12
+            // 
+            this.textBox12.Location = new System.Drawing.Point(419, 208);
+            this.textBox12.Name = "textBox12";
+            this.textBox12.Size = new System.Drawing.Size(76, 20);
+            this.textBox12.TabIndex = 80;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(339, 213);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(74, 13);
+            this.label12.TabIndex = 79;
+            this.label12.Text = "Hora Estudio3";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(538, 119);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(168, 54);
+            this.button3.TabIndex = 81;
+            this.button3.Text = "Consultar informe";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(15, 247);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(773, 191);
+            this.dataGridView1.TabIndex = 82;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox12);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.textBox10);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox14);
+            this.Controls.Add(this.label22);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox14;
+        private System.Windows.Forms.Label label22;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox10;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox textBox12;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ IncidenciasGarum/ServicioWeb.EstudioAjustes/Form1.cs
@@ -0,0 +1,284 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Npgsql;
+
+namespace ServicioWeb.EstudioAjustes
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public NpgsqlConnection conexionabbdd(string ip, string usuario, string password, string puerto, string database)
+        {
+            NpgsqlConnection conexion = new NpgsqlConnection();
+            var cadena_conexion = "Server=" + ip + ";port=" + puerto + ";user Id=" + usuario + ";password=" + password + ";Database=" + database;
+
+            if (!string.IsNullOrEmpty(cadena_conexion))
+            {
+                try
+                {
+                    conexion = new NpgsqlConnection(cadena_conexion);
+                    conexion.Open();
+                }
+                catch
+                {
+                    conexion.Close();
+                }
+
+            }
+            return conexion;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string parames = ConfigurationManager.AppSettings["es"];
+            string paramurl = ConfigurationManager.AppSettings["urlws"];
+            string paramhoraestudio1 = ConfigurationManager.AppSettings["horaestudio1"];
+            string paramhoraestudio2 = ConfigurationManager.AppSettings["horaestudio2"];
+            string paramhoraestudio3 = ConfigurationManager.AppSettings["horaestudio3"];
+            
+            string paramipbbdd = ConfigurationManager.AppSettings["ipbbdd"];
+            string paramusuario = ConfigurationManager.AppSettings["usuario"];
+            string parampassword = ConfigurationManager.AppSettings["password"];
+
+            string parampuerto = ConfigurationManager.AppSettings["puerto"];
+            string parambasededatos = ConfigurationManager.AppSettings["basededatos"];
+            
+            //string miValor = ConfigurationManager.AppSettings["xadinput"];
+            textBox3.Text = DateTime.Now.ToString("hh:mm:ss");
+            textBox4.Text = paramhoraestudio1;
+            textBox10.Text = paramhoraestudio2;
+            textBox12.Text = paramhoraestudio3;
+            textBox6.Text = paramurl;
+            textBox14.Text = parames;
+            textBox1.Text = paramipbbdd;
+            textBox2.Text = paramusuario;
+            textBox6.Text = paramurl;
+            textBox8.Text = parampassword;
+            textBox9.Text = parampuerto;
+            textBox5.Text = parambasededatos;
+
+
+            
+            // rellenamos por defecto los textos de input y export si estan vacios
+            if (string.IsNullOrEmpty(textBox1.Text.Trim()))
+            {
+                textBox1.Text = "10.0.0.101";
+            }
+            if (string.IsNullOrEmpty(textBox2.Text.Trim().Trim()))
+            {
+                textBox2.Text = "postgres";
+            }
+            if (string.IsNullOrEmpty(textBox8.Text.Trim()))
+            {
+                textBox8.Text = "postgres";
+            }
+            if (string.IsNullOrEmpty(textBox5.Text.Trim().Trim()))
+            {
+                textBox5.Text = "tpv";
+            }
+            if (string.IsNullOrEmpty(textBox6.Text.Trim().Trim()))
+            {
+                textBox6.Text = "http://2.139.147.209:1602/";
+            }
+            if (string.IsNullOrEmpty(textBox14.Text.Trim().Trim()))
+            {
+                textBox14.Text = "Estacion de servicio PRUEBAS";
+            }
+            if (string.IsNullOrEmpty(textBox9.Text.Trim().Trim()))
+            {
+                textBox9.Text = "5432";
+            }
+
+            if (string.IsNullOrEmpty(textBox4.Text.Trim().Trim()))
+            {
+                textBox4.Text = "07:30:00";
+            }
+            if (string.IsNullOrEmpty(textBox10.Text.Trim().Trim()))
+            {
+                textBox10.Text = "15:30:00";
+            }
+            if (string.IsNullOrEmpty(textBox12.Text.Trim().Trim()))
+            {
+                textBox12.Text = "23:30:00";
+            }
+        }
+        
+     //   NpgsqlConnection conn = new NpgsqlConnection("Server=192.168.1.77;");
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var conexionbbdd = conexionabbdd(textBox1.Text, textBox2.Text, textBox8.Text, textBox9.Text, textBox5.Text);
+                var estadoconexionbbdd = conexionbbdd.State.ToString();
+                //MessageBox.Show(estadoconexion);
+                if (estadoconexionbbdd == "Open")
+                {
+                    textBox7.Text = "OK";
+                    textBox7.BackColor = Color.Green;
+                    conexionbbdd.Close();
+                    
+                }
+                else
+                {
+                    textBox7.Text = "kO";
+                    textBox7.BackColor = Color.Red;
+                }
+            }
+            catch {
+                
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Configuration config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+        
+            
+            try
+            {
+                config.AppSettings.Settings.Remove("es");
+                config.AppSettings.Settings.Add("es", textBox14.Text);
+                config.Save(ConfigurationSaveMode.Modified);
+            }
+            catch
+            {
+                config.AppSettings.Settings.Add("es", textBox14.Text);
+                config.Save(ConfigurationSaveMode.Modified);
+            }
+
+            try
+            {
+                config.AppSettings.Settings.Remove("urlws");
+                config.AppSettings.Settings.Add("urlws", textBox6.Text);
+                config.Save(ConfigurationSaveMode.Modified);
+            }
+            catch
+            {
+                config.AppSettings.Settings.Add("urlws", textBox6.Text);
+                config.Save(ConfigurationSaveMode.Modified);
+            }
+            try
+            {
+                config.AppSettings.Settings.Remove("horaestudio1");
+                config.AppSettings.Settings.Add("horaestudio1", textBox4.Text);
+                config.Save(ConfigurationSaveMode.Modified);
+            }
+            catch
+            {
+                config.AppSettings.Settings.Add("horaestudio1", textBox4.Text);
+                config.Save(ConfigurationSaveMode.Modified);
+            }
+            try
+            {
+                config.AppSettings.Settings.Remove("horaestudio2");
+                config.AppSettings.Settings.Add("horaestudio2", textBox10.Text);
+                config.Save(ConfigurationSaveMode.Modified);
+            }
+            catch
+            {
+                config.AppSettings.Settings.Add("horaestudio2", textBox10.Text);
+                config.Save(ConfigurationSaveMode.Modified);
+            }
+            try
+            {
+                config.AppSettings.Settings.Remove("horaestudio3");
+                config.AppSettings.Settings.Add("horaestudio3", textBox12.Text);
+                config.Save(ConfigurationSaveMode.Modified);
+            }
+            catch
+            {
+                config.AppSettings.Settings.Add("horaestudio3", textBox12.Text);
+                config.Save(ConfigurationSaveMode.Modified);
+            }
+
+            
+            try
+            {
+                config.AppSettings.Settings.Remove("ipbbdd");
+                config.AppSettings.Settings.Add("ipbbdd", textBox1.Text);
+                config.Save(ConfigurationSaveMode.Modified);
+            }
+            catch
+            {
+                config.AppSettings.Settings.Add("ipbbdd", textBox1.Text);
+                config.Save(ConfigurationSaveMode.Modified);
+            }
+
+            try
+            {
+                config.AppSettings.Settings.Remove("usuario");
+                config.AppSettings.Settings.Add("usuario", textBox2.Text);
+                config.Save(ConfigurationSaveMode.Modified);
+            }
+            catch
+            {
+                config.AppSettings.Settings.Add("usuario", textBox2.Text);
+                config.Save(ConfigurationSaveMode.Modified);
+            }
+
+            try
+            {
+                config.AppSettings.Settings.Remove("password");
+                config.AppSettings.Settings.Add("password", textBox8.Text);
+                config.Save(ConfigurationSaveMode.Modified);
+            }
+            catch
+            {
+                config.AppSettings.Settings.Add("password", textBox8.Text);
+                config.Save(ConfigurationSaveMode.Modified);
+            }
+            try
+            {
+                config.AppSettings.Settings.Remove("puerto");
+                config.AppSettings.Settings.Add("puerto", textBox9.Text);
+                config.Save(ConfigurationSaveMode.Modified);
+            }
+            catch
+            {
+                config.AppSettings.Settings.Add("puerto", textBox9.Text);
+                config.Save(ConfigurationSaveMode.Modified);
+            }
+            try
+            {
+                config.AppSettings.Settings.Remove("basededatos");
+                config.AppSettings.Settings.Add("basededatos", textBox5.Text);
+                config.Save(ConfigurationSaveMode.Modified);
+            }
+            catch
+            {
+                config.AppSettings.Settings.Add("basededatos", textBox5.Text);
+                config.Save(ConfigurationSaveMode.Modified);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+          //  dataGridView1.DataSource = consultarinforme();
+            textBox12.Text = consultarinforme();
+
+        }
+        public int consultarinforme()
+        {
+            NpgsqlConnection conn = new NpgsqlConnection("Server=" + textBox1.Text + ";port=" + textBox9.Text + ";user Id=" + textBox2.Text + ";password=" + textBox8.Text + ";Database=" + textBox5.Text);
+            string consulta = "select max(id_informe) as id_informe from \"informe\" order by id_informe asc";
+            NpgsqlCommand conector = new NpgsqlCommand(consulta, conn);
+            NpgsqlDataAdapter datos = new NpgsqlDataAdapter(conector);
+            DataTable informes = new DataTable();
+            datos.Fill(informes);
+            return informes.id_informe;
+        }
+       
+    }
+}
